a16ea2d32d96331aa4d5f5c91efec4ef63fe50e2
Luka Kraljević <luka.kraljevic90@gmail.com>
2017-10-24 13:51:43 +0200

    Sentry upload

--- www/raven.js
+++ www/raven.js
@@ -1,1 +1,18 @@
-Raven.config('https://55d103f204fe4cea9dafa879b44dcc53@sentry.io/234408',{release: '6.1.1'}).install()
+Raven.config('https://55d103f204fe4cea9dafa879b44dcc53@sentry.io/234408',{release: 'test'}).install()
+
+/*
+const dsn = 'https://55d103f204fe4cea9dafa879b44dcc53@sentry.io/234408';
+Raven.config(dsn, {
+  dataCallback(data) {
+    const normalize = filename => filename.split('/www/', 2)[1]
+
+    data.exception.values[0].stacktrace.frames.forEach(frame => {
+      frame.filename = normalize(frame.filename)
+    })
+
+    data.culprit = data.exception.values[0].stacktrace.frames[0].filename
+
+    return data
+  }
+})
+*/
